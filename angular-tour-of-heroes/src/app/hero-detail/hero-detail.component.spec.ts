2bb05e89e23327c3642c6803a657ddd1ae53404d
Kristian Fischer <khfishy@gmail.com>
2019-08-19 10:04:59 -0400

    Fixed unit tests.

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.spec.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
 
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from '../heroes/heroes.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
@@ -8,7 +12,8 @@ describe('HeroDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroDetailComponent ]
+      imports: [FormsModule, AppRoutingModule],
+      declarations: [HeroDetailComponent, HeroesComponent, DashboardComponent]
     })
     .compileComponents();
   }));
